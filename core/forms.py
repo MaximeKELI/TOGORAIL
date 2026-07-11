20da8935a2c1208781598af445adb46be78d2e77
MaximeKELI <presidentnetero01@gmail.com>
2026-07-11 17:46:20 +0000

    Recuperation de compte

--- core/forms.py
+++ core/forms.py
@@ -1,4 +1,6 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.models import User
 from django.utils.translation import gettext_lazy as _
 
 from .models import ContactMessage, JobApplication, Subscriber
