55f3dbd656c09e5fcd26ed94a9ad25b08a10cfa9
MaximeKELI <presidentnetero01@gmail.com>
2026-07-11 16:50:00 +0000

    Recuperation de compte

--- config/urls.py
+++ config/urls.py
@@ -2,12 +2,29 @@ from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from django.http import HttpResponse
 from django.urls import include, path
+from django.views.generic import TemplateView
 
-# Non-translated URLs (admin, language switch endpoint)
+from core.sitemaps import sitemaps
+
+
+def robots_txt(request):
+    lines = [
+        "User-agent: *",
+        "Allow: /",
+        f"Sitemap: {request.scheme}://{request.get_host()}/sitemap.xml",
+    ]
+    return HttpResponse("\n".join(lines), content_type="text/plain")
+
+
+# Non-translated URLs (admin, language switch endpoint, SEO files)
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("i18n/", include("django.conf.urls.i18n")),
+    path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="django.contrib.sitemaps.views.sitemap"),
+    path("robots.txt", robots_txt, name="robots_txt"),
 ]
 
 # Language-prefixed URLs (/fr/..., /en/...)
